58a470f72d426af6aecb100b01d396049ab1259d
ya1515468-star <ya1515468@gmail.com>
2026-09-22 00:03:32 +0300

    chore: remove unused chat lobby import

--- lib/features/chat/presentation/pages/chat_rooms_page.dart
+++ lib/features/chat/presentation/pages/chat_rooms_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'chat_lobby_page.dart';
 
 class ChatRoomsPage extends ConsumerWidget {
   const ChatRoomsPage({super.key});

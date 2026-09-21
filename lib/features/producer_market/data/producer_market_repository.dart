c257cbfb8a2026d2b5268061c248dc95a57784be
ya1515468-star <ya1515468@gmail.com>
2026-09-21 22:00:10 +0300

    fix: import storage session helper

--- lib/features/producer_market/data/producer_market_repository.dart
+++ lib/features/producer_market/data/producer_market_repository.dart
@@ -1,4 +1,5 @@
 import 'package:file_picker/file_picker.dart';
+import '../../../core/services/supabase_service.dart';
 import 'package:flutter/foundation.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uuid/uuid.dart';

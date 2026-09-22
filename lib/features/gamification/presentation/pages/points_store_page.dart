a89ba9043aa0753945828bdc6fa1b5d52c0d42f4
ya1515468-star <ya1515468@gmail.com>
2026-09-22 13:46:15 +0300

    fix: restore shared store feature type contract

--- lib/features/gamification/presentation/pages/points_store_page.dart
+++ lib/features/gamification/presentation/pages/points_store_page.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:uuid/uuid.dart';
+
 import '../../../../core/services/media_upload_service.dart';
+
+enum StoreFeatureType { glow, frame, background }
 
 class PointsStorePage extends StatefulWidget {
   const PointsStorePage({super.key});
